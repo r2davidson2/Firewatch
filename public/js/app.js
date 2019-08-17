48ea987c9fc03b0d6ac7aa33f0c193795f4cd181
TR-1000 <t.l.ross@outlook.com>
2019-08-17 08:19:17 -0500

    map api is working inside index.html

--- public/js/app.js
+++ public/js/app.js
@@ -1,29 +1,20 @@
+const app = angular.module('MyApp', []);
 
+app.controller("gooleMapController", ['$http', "$scope", "$sce", function($http, $scope, $sce) {
 
-const app = angular.module('MyApp', []).config(function($sceDelegateProvider) {
-  $sceDelegateProvider.resourceUrlWhitelist([
-    // Allow same origin resource loads.
-    'self',
-    // Allow loading from our assets domain.  Notice the difference between * and **.
-    'https://www.google.com/maps/embed/v1/place'
-  ]);
-});
-app.controller("MainController", ['$http', function($http) {
-
-
-  this.getKey = () => {
-    $http({
-      method: "GET",
-      url: "/config",
-    }).then ((response) => {
-      this.url = `https://www.google.com/maps/embed/v1/place?q=47.5951518%2C-122.3316393&key=`
-      this.key = response.data;
-      this.sourceUrl = `${this.url}${this.key}`
-      console.log("This is the source url for the iframe", this.sourceUrl);
+  $scope.trustAsUrl = (url) => {
+    return $sce.trustAsResourceUrl(url)
+  }
+    this.getKey = () => {
+      $http({
+        method: "GET",
+        url: "/config",
+      }).then ((response) => {
+        this.key = response.data;
+        this.url = "https://www.google.com/maps/embed/v1/place?q=47.5951518%2C-122.3316393&key="
+        this.src = this.url+this.key
       });
     }
 
-  this.getKey()
-
-
+    this.getKey()
 }]);
